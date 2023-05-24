cf18a64693293e88db407b0c2eb61682760b873e
Vladyslav <vladsheln58@gmail.com>
2023-05-24 23:11:39 +0300

    2

--- integral.cxx
+++ integral.cxx
@@ -34,8 +34,14 @@ float integral (float (*f)(float), float xmin, float xmax, float ymin, float yma
   std::cout<<(xmax - xmin)*(ymax - ymin)*density<<std::endl;
 }
 
-
-int main(){
+int main(int argc, char* argv[]){
+  float min=-2;
+  float max=2;
+  if (argc==3)
+  {
+    min=atof(argv[1]);
+    max=atof(argv[2]);
+  }
   std::cout<< "RESULT: " <<std::endl;
-  integral(f,-2,2,0,4);
+  integral(f,min,max,0,4);
 }
